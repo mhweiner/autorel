fcbf39c51c5f861157c6fe3746466e19975a8cdf
Marc H. Weiner <mhweiner234@gmail.com>
2024-06-28 16:41:43 -0400

    fix: test

--- src/lib/bash.ts
+++ src/lib/bash.ts
@@ -1,8 +1,11 @@
 import {execSync} from 'child_process';
+import out from './output';
 
 export function $(strings: TemplateStringsArray, ...values: any[]): string {
 
     const command = strings.reduce((acc, str, i) => acc + str + (values[i] || ''), '');
+
+    out.debug(command);
 
     const escapedCommand = command.replace(/(["$`\\])/g, '\\$1').replace(/\n/g, '\\n');
     const output = execSync(`bash -c "${escapedCommand}"`, {encoding: 'utf8'});
